678fb2b33883d3e32f71265b72cdb6a7c523eece
pauloskaterock <pauloengenharia5@yahoo.com>
2024-05-20 18:49:47 -0300

    Processamento de Documentos PDF with Python-Botcity

--- Python-RPA-Botcity/lerPDF/bot.py
+++ Python-RPA-Botcity/lerPDF/bot.py
@@ -1,6 +1,14 @@
 
-# Import for the Desktop Bot
-# from botcity.document_processing import *
+ #Import for the Desktop Bot
+ #from botcity.document_processing import *
+ #import pathlib
+#import panda as pd
+
+
+
+dados = []
+
+
 
 def lerPDF():
     reader = PDFReader()
@@ -14,28 +22,35 @@ def lerPDF():
     parser = PDFReader().read_file('docs\docs\Contoso_INVOICE_TailSpin.pdf')
    
     _date = parser.get_first_entry("Date:")
-    value = parser.read(_date, 1.380952, -2.1, 3.047619, 3.5)
-    print('Date: ' + value)
+    date = parser.read(_date, 1.380952, -2.1, 3.047619, 3.5)
+    print('Date: ' + date)
     
     _bill_to = parser.get_first_entry("Bill to:")
-    value = parser.read(_bill_to, 1.148148, -2.3, 6.259259, 4.4)
-    print('Bill to: ' + value)
+    bill_to = parser.read(_bill_to, 1.148148, -2.3, 6.259259, 4.4)
+    print('Bill to: ' + bill_to)
 
     _contact = parser.get_first_entry("Contact:")
-    value = parser.read(_contact, 1.171053, -1.6, 4.973684, 3.8)
-    print('Contact: ' + value)
+    contact = parser.read(_contact, 1.171053, -1.6, 4.973684, 3.8)
+    print('Contact: ' + contact)
 
     _balance_due = parser.get_first_entry("Balance due:")
-    value = parser.read(_balance_due, 1.133333, -1.833333, 1.766667, 3.583333)
-    print('Balance due: ' + value)
+    balance_due = parser.read(_balance_due, 1.133333, -1.833333, 1.766667, 3.583333)
+    print('Balance due: ' + balance_due)
 
-lerPDF()
-    
-    
-    
-    
-    
+    arquivos = pathlib.Path('./docs/Contoso_INVOICE_TailSpin.pdf').glob('*.pdf')
 
+    for arquivo in arquivos:
+       # print(arquivo)
+        lerPDF(arquivo)
+
+
+dados.append([date, bill_to, contact, balance_due])
+# lerPDF()
+    
+    
+    
+df = pd.DataFrame(dados, columns=['Date' , 'Bill to', 'Contact', 'Balance due'])
+df.to_csv('dados_pdf.csv', sep=',', index=False)
 
 
 
