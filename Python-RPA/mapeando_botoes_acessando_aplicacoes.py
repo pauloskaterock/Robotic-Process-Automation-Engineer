4281b61807548e8a225af1c66e99e57b9f32cf29
pauloskaterock <pauloengenharia5@yahoo.com>
2024-06-08 19:00:18 -0300

    Update

--- Python-RPA/mapeando_botoes_acessando_aplicacoes.py
+++
@@ -1,9 +0,0 @@
-import pyautogui as pyag
-
-from time import sleep
-foto_python = pyag.position()
-botao_fechar = pyag.position()
-from pyautogui import click, doubleClick
-from pyautogui import click, moveTo
-moveTo(foto_python, duration=1); sleep(0.5); doubleClick(); sleep(2); moveTo(botao_fechar, duration=0.5);click()
-
